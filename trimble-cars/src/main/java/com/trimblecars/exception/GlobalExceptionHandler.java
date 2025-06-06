e52010f9a3b99ac6e718ffa1bc2a6f588ed42f6a
Kamdrathisurekha <surekhakamdrathi123@gmail.com>
2025-06-06 23:17:06 +0530

    Update GlobalExceptionHandler.java

--- trimble-cars/src/main/java/com/trimblecars/exception/GlobalExceptionHandler.java
+++ trimble-cars/src/main/java/com/trimblecars/exception/GlobalExceptionHandler.java
@@ -16,30 +16,7 @@ import java.util.*;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-   
-   /* @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<?> handleValidationErrors(MethodArgumentNotValidException ex) {
-        Map<String, String> errors = new HashMap<>();
-        ex.getBindingResult().getFieldErrors().forEach(error ->
-                errors.put(error.getField(), error.getDefaultMessage()));
-        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
-    }
-
- 
-    @ExceptionHandler(HttpMessageNotReadableException.class)
-    public ResponseEntity<?> handleEnumErrors(HttpMessageNotReadableException ex) {
-        if (ex.getCause() instanceof InvalidFormatException invalidEx) {
-            if (invalidEx.getTargetType().isEnum()) {
-                String fieldName = invalidEx.getPath().get(0).getFieldName();
-                String accepted = Arrays.toString(invalidEx.getTargetType().getEnumConstants());
-                String message = String.format("Invalid value for '%s'. Allowed values are: %s", fieldName, accepted);
-                return new ResponseEntity<>(Map.of("error", message), HttpStatus.BAD_REQUEST);
-            }
-        }
-        return new ResponseEntity<>(Map.of("error", "Invalid request."), HttpStatus.BAD_REQUEST);
-    }*/
-	
-	
+  
 	@ExceptionHandler(IllegalStateException.class)
 	public ResponseEntity<?> handleIllegalState(IllegalStateException ex) {
 	    return ResponseEntity.badRequest().body(Map.of("error", ex.getMessage()));
